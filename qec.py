74d784985f75a2b6a975080768727c3e193b7336
Boldi <boldar99@gmail.com>
2025-10-14 11:36:31 +0100

    qec.py file containing handy functions for QEC codes

---
+++ qec.py
@@ -0,0 +1,47 @@
+from typing import Optional
+
+from pyzx.graph import Graph
+from pyzx.graph.base import BaseGraph, VT, ET
+from pyzx.utils import VertexType
+
+
+def css_encoder(stabilizers: list[list[int]],
+                logicals: list[list[int]],
+                backend: Optional[str] = None) -> BaseGraph[VT, ET]:
+    n, k = len(stabilizers[0]), len(logicals[0])
+
+    num_internals_vertices = len(stabilizers) + len(logicals)
+    spacing = [(k + .5) * (n - 1) / num_internals_vertices for k in range(num_internals_vertices)]
+
+    encoder = Graph(backend)
+    inputs: list[VT] = []
+    outputs: list[VT] = []
+    x_vertices, z_vertices = [], []
+
+    for i, s in enumerate(logicals):
+        v1 = encoder.add_vertex(ty=VertexType.BOUNDARY, qubit=spacing[i], row=0)
+        v2 = encoder.add_vertex(ty=VertexType.Z, qubit=spacing[i], row=1)
+        encoder.add_edge((v1, v2))
+        inputs.append(v1)
+        z_vertices.append(v2)
+
+    for i in range(n):
+        v1 = encoder.add_vertex(ty=VertexType.X, qubit=i, row=3)
+        v2 = encoder.add_vertex(ty=VertexType.BOUNDARY, qubit=i, row=4)
+        encoder.add_edge((v1, v2))
+        x_vertices.append(v1)
+        outputs.append(v2)
+
+    for i, s in enumerate(stabilizers):
+        v2 = encoder.add_vertex(ty=VertexType.Z, qubit=spacing[len(logicals) + i], row=1)
+        z_vertices.append(v2)
+
+    for i, v in enumerate(logicals + stabilizers):
+        for j, val in enumerate(v):
+            if val == 1:
+                encoder.add_edge((z_vertices[i], x_vertices[j]))
+
+    encoder.set_inputs(tuple(inputs))
+    encoder.set_outputs(tuple(outputs))
+
+    return encoder
